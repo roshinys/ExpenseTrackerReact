e5544cc04945be905eb8992b924e13a10be84611
roshinys <roshinchaya@gmail.com>
2023-06-25 11:17:25 +0530

    added Filtered HtmlCss

--- src/Components/Expenses/Expense.js
+++ src/Components/Expenses/Expense.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expense.css";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
 
 function Expense(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   const expenses = [
     {
       id: "e1",
@@ -37,6 +44,10 @@ function Expense(props) {
 
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {expenses.map((expense) => {
         return (
           <ExpenseItem
