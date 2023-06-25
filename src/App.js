4672c9b52a381b056b76404165b3af894d271ff8
roshinys <roshinchaya@gmail.com>
2023-06-25 11:42:03 +0530

    rendered new expense using map and keys

--- src/App.js
+++ src/App.js
@@ -2,17 +2,50 @@ import { useState } from "react";
 import Expense from "./Components/Expenses/Expense";
 import NewExpense from "./Components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    location: "Mysore",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    location: "mumbai",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294,
+    date: new Date(2021, 2, 28),
+    location: "Bengaluru",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    location: "Chennai",
+  },
+];
+
 function App() {
-  const [expense, setExpense] = useState({});
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expenseData) => {
-    setExpense(expenseData);
+    setExpenses((prevState) => {
+      return [expenseData, ...prevState];
+    });
   };
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expense expense={expense}></Expense>
+      <Expense expenses={expenses}></Expense>
     </div>
   );
 }
--- src/Components/Expenses/Expense.js
+++ src/Components/Expenses/Expense.js
@@ -11,44 +11,13 @@ function Expense(props) {
     setFilteredYear(selectedYear);
   };
 
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: "Mysore",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      location: "mumbai",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294,
-      date: new Date(2021, 2, 28),
-      location: "Bengaluru",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location: "Chennai",
-    },
-  ];
-
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expenses.map((expense) => {
+      {props.expenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
